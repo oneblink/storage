4af2f4e1850f37411ac974f76574afd0d9299816
Matt Carroll <matt.carroll139@gmail.com>
2024-07-01 15:51:40 +1000

    [RELEASE] 2.1.0-beta.2

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as OneBlinkStorageError } from './OneBlinkStorageError'
 export { default as OneBlinkUploader } from './OneBlinkUploader'
+export { default as OneBlinkDownloader } from './OneBlinkDownloader'
 export * from './types'
